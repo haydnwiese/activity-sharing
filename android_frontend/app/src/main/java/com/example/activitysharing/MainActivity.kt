a930712bad207968b9b92f332205457f40102f25
Haydn Wiese <haydnwiese@gmail.com>
2021-02-28 19:43:49 -0500

    Add basic navigation

--- android_frontend/app/src/main/java/com/example/activitysharing/MainActivity.kt
+++ android_frontend/app/src/main/java/com/example/activitysharing/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.activitysharing
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.activitysharing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
     }
 }

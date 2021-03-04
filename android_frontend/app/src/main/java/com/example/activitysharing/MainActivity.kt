20e2b65b0e2dd609e5d2693f334b5101a0f9ad08
haydnwiese <45501692+haydnwiese@users.noreply.github.com>
2021-03-03 23:57:06 -0500

    Fix navigation issue

--- android_frontend/app/src/main/java/com/example/activitysharing/MainActivity.kt
+++ android_frontend/app/src/main/java/com/example/activitysharing/MainActivity.kt
@@ -2,20 +2,23 @@ package com.example.activitysharing
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.activitysharing.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
         NavigationUI.setupWithNavController(binding.bottomNav, navController)
     }
 }

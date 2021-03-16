6a2c8db4124691df0e25ba804485753364b71d5a
Haydn Wiese <haydnwiese@gmail.com>
2021-03-15 23:01:58 -0400

    Clean up navigation graph

--- android_frontend/app/src/main/java/com/example/activitysharing/ui/home/HomeFragment.kt
+++ android_frontend/app/src/main/java/com/example/activitysharing/ui/home/HomeFragment.kt
@@ -1,23 +1,12 @@
 package com.example.activitysharing.ui.home
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.example.activitysharing.R
-import com.example.activitysharing.ui.login.LoginViewModel
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
 
 class HomeFragment : Fragment() {
 

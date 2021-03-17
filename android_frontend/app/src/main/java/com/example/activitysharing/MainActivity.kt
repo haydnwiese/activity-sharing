ce8e75383c52f1b2e22486ce7854ae2a72ae2ec6
Haydn Wiese <haydnwiese@gmail.com>
2021-03-16 22:37:46 -0400

    Move top bar to fragment

--- android_frontend/app/src/main/java/com/example/activitysharing/MainActivity.kt
+++ android_frontend/app/src/main/java/com/example/activitysharing/MainActivity.kt
@@ -15,7 +15,7 @@ import com.example.activitysharing.databinding.ActivityMainBinding
 import timber.log.Timber
 
 
-class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -23,19 +23,5 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        navController.addOnDestinationChangedListener(this)
     }
-
-    override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
-        val visibility = when(destination.id) {
-            R.id.loginFragment -> View.GONE
-            else -> View.VISIBLE
-        }
-        binding.topAppBar.visibility = visibility
-    }
-
 }

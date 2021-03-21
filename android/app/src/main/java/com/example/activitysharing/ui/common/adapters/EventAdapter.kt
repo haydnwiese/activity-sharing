ea546665a71f37ef9d2b04a0d3b6833be5c167b1
haydnwiese <haydnwiese@gmail.com>
2021-03-20 23:11:44 -0400

    WIP: Implement adapter and viewholder

--- android/app/src/main/java/com/example/activitysharing/ui/common/adapters/EventAdapter.kt
+++ android/app/src/main/java/com/example/activitysharing/ui/common/adapters/EventAdapter.kt
@@ -1,6 +1,24 @@
 package com.example.activitysharing.ui.common.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate
 import com.example.activitysharing.data.model.Event
+import com.example.activitysharing.databinding.ListItemEventBinding
+import com.example.activitysharing.ui.common.view_holders.EventViewHolder
 
-class EventAdapter(private val dataSet: Event) {
+class EventAdapter(private val dataSet: List<Event>): RecyclerView.Adapter<EventViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        val binding = ListItemEventBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return EventViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        holder
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
--- android/app/src/main/java/com/example/activitysharing/ui/common/view_holders/EventViewHolder.kt
+++ android/app/src/main/java/com/example/activitysharing/ui/common/view_holders/EventViewHolder.kt
@@ -1,4 +1,10 @@
 package com.example.activitysharing.ui.common.view_holders
 
-class EventViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.activitysharing.data.model.Event
+import com.example.activitysharing.databinding.ListItemEventBinding
+
+class EventViewHolder(private val binding: ListItemEventBinding) : RecyclerView.ViewHolder(binding.root) {
+
 }
